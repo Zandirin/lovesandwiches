c39a8fb4d43b73f66401b81338db51b68f95d469
Zandirin <richardjohnkraft@gmail.com>
2023-06-06 21:59:51 +0100

    Adds stock sheet update function

--- run.py
+++ run.py
@@ -88,6 +88,21 @@ def get_last_5_sales_entries():
     
     return columns
 
+def calculate_stock_data(data):
+    """
+    Calculates data from stock sheet. Returns average
+    """
+    print("Calculating stock data...\n")
+    new_stock_data = []
+
+    for column in data:
+        int_column = [int(num) for num in column]
+        average = sum(int_column) / len(int_column)
+        stock_num = average * 1.1
+        new_stock_data.append(round(stock_num))
+    
+    return new_stock_data
+
 def main():
     """
     Main function of the program, calls all other functions and runs them to update the spreadsheet.
@@ -97,8 +112,10 @@ def main():
     update_worksheet(sales_data, 'sales')
     new_surplus_data = calculate_surplus_data(sales_data)
     update_worksheet(new_surplus_data, 'surplus')
+    sales_columns = get_last_5_sales_entries()
+    stock_data = calculate_stock_data(sales_columns)
+    update_worksheet(stock_data, 'stock')
 
 print("Welcome to Love Sandwiches Data Automation")
-# main()
+main()
 
-sales_columns = get_last_5_sales_entries()
