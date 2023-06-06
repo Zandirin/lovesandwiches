457e4849267483fea9f2a58406c5166691a61ef3
Zandirin <richardjohnkraft@gmail.com>
2023-06-06 21:23:03 +0100

    Adds surplus sheet update function

--- run.py
+++ run.py
@@ -57,6 +57,15 @@ def update_sales_worksheet(data):
     sales_worksheet.append_row(data)
     print("Sales worksheet updated successfully!\n")
 
+def update_surplus_worksheet(data_surplus):
+    """
+    Using the surplus data calculation, adds data to surplus worksheet
+    """
+    print("Updating surplus worksheet...\n")
+    surplus_worksheet = SHEET.worksheet('surplus')
+    surplus_worksheet.append_row(data_surplus)
+    print("Surplus worksheet updated successfully!\n")
+
 def calculate_surplus_data(sales_row):
     """
     Calculates the surplus stock for each item type.
@@ -82,6 +91,7 @@ def main():
     sales_data = [int(num) for num in data]
     update_sales_worksheet(sales_data)
     new_surplus_data = calculate_surplus_data(sales_data)
+    update_surplus_worksheet(new_surplus_data)
 
 print("Welcome to Love Sandwiches Data Automation")
 main()
